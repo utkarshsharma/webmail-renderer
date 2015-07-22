95a4abe8f099f49f2f9977f4bfcadb14a36add26
utkarshsharma <utkarsh.hs@gmail.com>
2015-07-22 19:09:15 +0530

    Working with Preview Manager now

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var util = require('util'),
     exec = require('child_process').exec,
     child;
 
-child = exec('java -jar selenium-server-standalone-2.45.0.jar')
-
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var path = require ('path')
 var webdriverio = require ('webdriverio')
 var fs = require ('fs');
+var fs1 = require ('fs');
 
 var homedir = process.env['HOME']
 var nodemailer = require('nodemailer');
@@ -18,17 +18,13 @@ var usr = obj.receivers.gmail.username
 var pwd = obj.receivers.gmail.password
 var usr_yahoo = obj.receivers.yahoo.username
 var pwd_yahoo = obj.receivers.yahoo.password
-//usr_yahoo = usr_yahoo + '@yahoo.com'
-
 var usr_send = obj.sender.username
 var pwd_send = obj.sender.password
 
 usr_send = usr_send + '@gmail.com'
 
-var filepath1 = homedir + '/gmailscreenshot'
-//var filepath2 = 'file://' + filepath1
-var filepath3 = homedir + '/yahooscreenshot'
-//var filepath4 = 'file://' + filepath3
+var filepath_gmail = homedir + '/gmailscreenshot.png'
+var filepath_yahoo = homedir + '/yahooscreenshot.png'
 
 var options = {
     desiredCapabilities: {
@@ -37,13 +33,23 @@ var options = {
 };
 var client = webdriverio.remote(options);
 var http = require ('http');
-var url = obj.url;
+
+var claimURL = 'http://0.0.0.0:3000/api/PreviewTasks/claim?renderer=gmail'
 
 var time= new Date().getTime();
 
+//********************Main function here***************************************************
+
+child = exec('cd '+ homedir + '&& java -jar selenium-server-standalone-2.45.0.jar');
+
+setTimeout( function(){
+	poll();
+}, 20000)
+//*****************************************************************************************
+
 function poll() {
-	http.get(url, function(res) {
-	    var body = '';
+	http.get(claimURL, function(res) {
+		var body = '';
 	    res.on('data', function(chunk) {
 	        body += chunk;
 	    });
@@ -51,31 +57,27 @@ function poll() {
 	    res.on('end', function() {
 	        var Email = JSON.parse(body)
 	     	var newTask = Email.newTask;
-	     	if (newTask==1){
-	     		render();
+	     	if (newTask != null){
+     			render(newTask);
+				console.log('Task ' + newTask.id + ' picked');
+	     	}
+	     	else {
+				console.log('No tasks pending');
+		     	setTimeout( function(){
+		     		poll();
+		     	}, 60000)
 	     	}
 	     });
 	});
 }
 
-poll();
+function render(Task) {
 
-function render() {
-	var http = require ('http');
-	http.get(url, function(res) {
-	    var body = '';
-	    res.on('data', function(chunk) {
-	        body += chunk;
-	    });
-
-	    res.on('end', function() {
-	        var Email = JSON.parse(body)
-
-			var subj = Email.subject
-			var htmlEmail = Email.htmlmessage
-			var textEmail = Email.textmessage
-			var gmail = Email.render_with.gmail;
-			var yahoo = Email.render_with.yahoo;
+			//var subj = Task.subject
+			//we need to add a subject to the message field of the preview batch and preview tasks.
+			var htmlEmail = Task.message
+			var batchId = Task.batchId
+			//var textEmail = Task.textmessage
 			var transporter = nodemailer.createTransport({
 			    service: 'gmail',
 			    auth: {
@@ -87,55 +89,72 @@ function render() {
 			transporter.sendMail({
 			    from: usr_send,
 			    to: usr + '@gmail.com',
-			    bcc: usr_yahoo,
-			    subject: subj,
-			    text: textEmail,
+			    //bcc: usr_yahoo,
+			    subject: 'subj',
+			    text: 'textEmail',
 			    html: htmlEmail
 			});
 			client
 				.init()
 				.windowHandleMaximize();
 
-			if (gmail==1) {
-				client		
+
+				client
+
+//***********************Gmail Part************************************************************************
 					.url ('https://gmail.com')
 					.waitFor('input[id="Email"]',5000)
-				    .setValue('input[id="Email"]', usr)
+				 	.setValue('input[id="Email"]', usr)
 					.click('#next')
 					.waitFor('input[type="password"]',5000)
-				    .setValue ('input[type="password"]', pwd)
-				    .click ('#signIn')
+				 	.setValue ('input[type="password"]', pwd)
+				 	.click ('#signIn')
 					.waitFor('input[aria-label="Search"]',20000)
 					.setValue('input[id="gbqfq"]', usr_send)
 					.click('#gbqfb')
 					.waitFor('input[aria-label="Search"]',20000)
 					.element('<body>').keys(['Down arrow','Enter'])
 					.waitFor('img[alt="In new window"]',10000)
-					.saveScreenshot(filepath1)
-					//.element('<body>').keys(['Control','t'])
-					//.url(filepath2);
-			}
-			if (yahoo==1) {
-				client
-				    .url ('https://mail.yahoo.com')
-				    .waitFor('input[name="username"]',5000)
-				    .setValue('input[name="username"]', usr_yahoo)
-				    .setValue ('input[name="passwd"]', pwd_yahoo)
-				    .click ('#login-signin')
-					.waitFor('input[placeholder="Search"]',10000)
-					.setValue('input[placeholder="Search"]', usr_send)
-					.element('<body>').keys('Enter')
-					.pause(10000)
-					.element('<body>').keys(['Down arrow'])
-					.element('<body>').keys(['Enter'])
-					.pause(5000)
-					.saveScreenshot(filepath3)
-					//.element('<body>').keys(['Control','t'])
-					//.url(filepath4)
-			}
+					.saveScreenshot(filepath_gmail)
+//*********************************************************************************************************
+
+//***************************Yahoo Part********************************************************************
+					// .url ('https://mail.yahoo.com')
+					// .waitFor('input[name="username"]',5000)
+					// .setValue('input[name="username"]', usr_yahoo)
+					// .setValue ('input[name="passwd"]', pwd_yahoo)
+					// .click ('#login-signin')
+					// .waitFor('input[placeholder="Search"]',10000)
+					// .setValue('input[placeholder="Search"]', usr_send)
+					// .element('<body>').keys('Enter')
+					// .pause(10000)
+					// .element('<body>').keys(['Down arrow'])
+					// .element('<body>').keys(['Enter'])
+					// .pause(5000)
+					// .saveScreenshot(filepath_yahoo)
+//*********************************************************************************************************
+
 			client.end(function() {
-				setTimeout(poll(),60000);
+				// convert image to base64 encoded string
+				setTimeout( function(){
+		     		var base64data = base64_encode(filepath_gmail);
+					// var base64data = base64_encode(filepath_yahoo);
+					var postURL = 'http://0.0.0.0:3000/api/PreviewTasks/update?where=%7B%22batchId%22%3A%22'+batchId+'%22%2C%22renderer%22%3A%22gmail%22%7D'
+					var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+					xmlhttp.open("POST", postURL);
+					xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+					xmlhttp.send(JSON.stringify(  {"result": base64data}));
+					setTimeout(poll(), 10000);
+		     	}, 5000)
 			});
-	    });
-	});
+	    
+	
 };
+
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}

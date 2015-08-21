ddc3adbf616cab948947dc055ad31c61b9cb24db
utkarshsharma <utkarsh.hs@gmail.com>
2015-08-21 23:34:31 +0530

    GSoC final commit

--- gmail.js
+++ gmail.js
@@ -3,28 +3,30 @@ var util = require('util'),
     child;
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var path = require ('path')
-var webdriverio = require ('webdriverio')
+var path = require ('path');
+var webdriverio = require ('webdriverio');
 var fs = require ('fs');
 var fs1 = require ('fs');
 
-var homedir = process.env['HOME']
+var homedir = process.env.HOME;
 var nodemailer = require('nodemailer');
 
 var loginDetails = fs.readFileSync('config.json');
 
 var obj = JSON.parse(loginDetails);
-var usr = obj.receivers.gmail.username
-var pwd = obj.receivers.gmail.password
-var usr_send = obj.sender.username
-var pwd_send = obj.sender.password
-var prevemURL = obj.prevemURL
-var prevemLoginEmail = obj.prevemCredentials.email
-var prevemLoginPass = obj.prevemCredentials.password
+var usr = obj.receivers.gmail.username;
+var pwd = obj.receivers.gmail.password;
+var usr_send = obj.sender.username;
+var pwd_send = obj.sender.password;
+var prevemURL = obj.prevemURL;
+var prevemLoginEmail = obj.prevemCredentials.email;
+var prevemLoginPass = obj.prevemCredentials.password;
 
-usr_send = usr_send + '@gmail.com'
+usr_send = usr_send + '@gmail.com';
 
-var filepath_gmail = homedir + '/gmailscreenshot.png'
+var filepath_gmail = homedir + '/gmailscreenshot.png';
+var batchId;
+var error = null;
 
 var options = {
     desiredCapabilities: {
@@ -34,11 +36,11 @@ var options = {
 var client = webdriverio.remote(options);
 var http = require ('http');
 
-var claimURL = prevemURL + '/api/PreviewTasks/claim?renderer=gmail'
+var claimURL = prevemURL + '/api/PreviewTasks/claim?renderer=gmail';
 
 //********************Initial function call here*********************************************
 var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-var loginURL = 'http://localhost:3000' + '/api/Users/login'
+var loginURL = 'http://localhost:3000' + '/api/Users/login';
 xmlhttp.open("POST", loginURL);
 xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 xmlhttp.onreadystatechange = function() {//Call a function when the state changes.
@@ -47,24 +49,24 @@ xmlhttp.onreadystatechange = function() {//Call a function when the state change
 		accessTokenUrlExtension = 'access_token=' + accessToken;
 		poll();
     }
-}
+};
 xmlhttp.send(JSON.stringify(  {"email":prevemLoginEmail , "password": prevemLoginPass, "ttl": 31556926}));
 //*****************************************************************************************
 
 function poll() {
 	var xmlhttp1 = new XMLHttpRequest();   // new HttpRequest instance 
 	// Checking if Selenium is running
-	var seleniumURL = 'http://localhost:4444/selenium-server/driver?cmd=getLogMessages'
+	var seleniumURL = 'http://localhost:4444/selenium-server/driver?cmd=getLogMessages';
 	xmlhttp1.open("GET", seleniumURL);
 	xmlhttp1.onreadystatechange = function() {
 	    if (this.readyState == this.DONE) {
 			if (this.responseText != 'OK,') {
 			//Selenium is not running. Starting selenium.
 	    		console.log("Starting Selenium Server");
-	    		child = exec('cd '+ homedir + '&& java -jar selenium-server-standalone-2.45.0.jar');
+	    		child = exec('selenium-standalone start');
 	    	}
 	    }
-	}
+	};
 	xmlhttp1.send();
 	setTimeout(function() {
 		http.get(claimURL + '&' + accessTokenUrlExtension, function(res) {
@@ -74,9 +76,9 @@ function poll() {
 		    });
 
 		    res.on('end', function() {
-		        var Email = JSON.parse(body)
+		        var Email = JSON.parse(body);
 		     	var newTask = Email.newTask;
-		     	if (newTask != null){
+		     	if (newTask !== null){
 	     			render(newTask);
 					console.log('Task ' + newTask.id + ' picked');
 		     	}
@@ -84,7 +86,7 @@ function poll() {
 					console.log('No tasks pending');
 			     	setTimeout( function(){
 			     		poll();
-			     	}, 60000)
+			     	}, 60000);
 		     	}
 		     });
 		});
@@ -92,10 +94,10 @@ function poll() {
 }
 
 function render(Task) {
-	var subj = Task.message.subject
-	var htmlEmail = Task.message.html
-	var batchId = Task.batchId
-	var textEmail = Task.message.text
+	var subj = Task.message.subject;
+	var htmlEmail = Task.message.html;
+	batchId = Task.batchId;
+	var textEmail = Task.message.text;
 	var transporter = nodemailer.createTransport({
 	    service: 'gmail',
 	    auth: {
@@ -119,24 +121,24 @@ function render(Task) {
         if (e.err){
         	if (e.err.code == 'ECONNREFUSED'){
 		    	client.end();
-		    	post_back('Connection') //Posting Connection to the result field of the task to tell that this is an "Connection Refused" kind of error.
+		    	post_back('Connection'); //Posting Connection to the result field of the task to tell that this is an "Connection Refused" kind of error.
 		    }
 		}
 	    else if (e.body) {
 		   	if (e.body.value.class === 'org.openqa.selenium.NoSuchElementException'){
 		   		client.end();
-		    	post_back('Element') //Posting Element to the result field of the task to tell that this is an "Element Not Found" kind of error.
+		    	post_back('Element'); //Posting Element to the result field of the task to tell that this is an "Element Not Found" kind of error.
 		    }
 	    }
 	    else {
 	    	client.end();
 	    	post_back('Unknown');
 	    }
-	})
+	});
 
 	client
 		.init()
-		.windowHandleMaximize();
+		.windowHandleMaximize()
 
 //***********************Gmail Part************************************************************************
 		.url ('https://gmail.com')
@@ -152,7 +154,7 @@ function render(Task) {
 		.waitFor('input[aria-label="Search"]',20000)
 		.element('<body>').keys(['Down arrow','Enter'])
 		.waitFor('img[alt="In new window"]',10000)
-		.saveScreenshot(filepath_gmail)
+		.saveScreenshot(filepath_gmail);
 //*********************************************************************************************************
 
 	client.end(function() {
@@ -162,9 +164,9 @@ function render(Task) {
 	     		var base64data = base64_encode(filepath_gmail);
 	     		post_back(base64data);
 	     	}
-     	}, 5000)
+     	}, 5000);
 	});
-};
+}
 
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
@@ -175,7 +177,7 @@ function base64_encode(file) {
 }
 
 function post_back(base64data) {
-	var postURL = prevemURL + '/api/PreviewTasks/update?where=%7B%22batchId%22%3A%22'+batchId+'%22%2C%22renderer%22%3A%22gmail%22%7D'
+	var postURL = prevemURL + '/api/PreviewTasks/update?where=%7B%22batchId%22%3A%22'+batchId+'%22%2C%22renderer%22%3A%22gmail%22%7D';
 	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
 	xmlhttp.open("POST", postURL + '&' + accessTokenUrlExtension);
 	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
--- yahoo.js
+++ yahoo.js
@@ -3,29 +3,28 @@ var util = require('util'),
     child;
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var path = require ('path')
-var webdriverio = require ('webdriverio')
+var path = require ('path');
+var webdriverio = require ('webdriverio');
 var fs = require ('fs');
 var fs1 = require ('fs');
 
-var homedir = process.env['HOME']
+var homedir = process.env.HOME;
 var nodemailer = require('nodemailer');
 
 var loginDetails = fs.readFileSync('config.json');
 
 var obj = JSON.parse(loginDetails);
-var usr_yahoo = obj.receivers.yahoo.username
-var pwd_yahoo = obj.receivers.yahoo.password
-var usr_send = obj.sender.username
-var pwd_send = obj.sender.password
-var prevemURL = obj.prevemURL
-var prevemLoginEmail = obj.prevemCredentials.email
-var prevemLoginPass = obj.prevemCredentials.password
+var usr_yahoo = obj.receivers.yahoo.username;
+var pwd_yahoo = obj.receivers.yahoo.password;
+var usr_send = obj.sender.username;
+var pwd_send = obj.sender.password;
+var prevemURL = obj.prevemURL;
+var prevemLoginEmail = obj.prevemCredentials.email;
+var prevemLoginPass = obj.prevemCredentials.password;
 
+usr_send = usr_send + '@gmail.com';
 
-usr_send = usr_send + '@gmail.com'
-
-var filepath_yahoo = homedir + '/yahooscreenshot.png'
+var filepath_yahoo = homedir + '/yahooscreenshot.png';
 var batchId;
 var error = null;
 
@@ -37,12 +36,11 @@ var options = {
 var client = webdriverio.remote(options);
 var http = require ('http');
 
-
-var claimURL = prevemURL + '/api/PreviewTasks/claim?renderer=yahoo'
+var claimURL = prevemURL + '/api/PreviewTasks/claim?renderer=yahoo';
 
 //********************Initial function call here*********************************************
 var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-var loginURL = 'http://localhost:3000' + '/api/Users/login'
+var loginURL = 'http://localhost:3000' + '/api/Users/login';
 xmlhttp.open("POST", loginURL);
 xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 xmlhttp.onreadystatechange = function() {//Call a function when the state changes.
@@ -51,24 +49,24 @@ xmlhttp.onreadystatechange = function() {//Call a function when the state change
 		accessTokenUrlExtension = 'access_token=' + accessToken;
 		poll();
     }
-}
+};
 xmlhttp.send(JSON.stringify(  {"email": prevemLoginEmail, "password": prevemLoginPass, "ttl": 31556926}));
 //*****************************************************************************************
 
 function poll() {
 	var xmlhttp1 = new XMLHttpRequest();   // new HttpRequest instance 
 	// Checking if Selenium is running
-	var seleniumURL = 'http://localhost:4444/selenium-server/driver?cmd=getLogMessages'
+	var seleniumURL = 'http://localhost:4444/selenium-server/driver?cmd=getLogMessages';
 	xmlhttp1.open("GET", seleniumURL);
 	xmlhttp1.onreadystatechange = function() {
 	    if (this.readyState == this.DONE) {
 			if (this.responseText != 'OK,') {
 			//Selenium is not running. Starting selenium.
 	    		console.log("Starting Selenium Server");
-	    		child = exec('cd '+ homedir + '&& java -jar selenium-server-standalone-2.45.0.jar');
+	    		child = exec('selenium-standalone start');
 	    	}
 	    }
-	}
+	};
 	xmlhttp1.send();
 	setTimeout(function() {
 		http.get(claimURL + '&' + accessTokenUrlExtension, function(res) {
@@ -78,9 +76,9 @@ function poll() {
 		    });
 
 		    res.on('end', function() {
-		        var Email = JSON.parse(body)
+		        var Email = JSON.parse(body);
 		     	var newTask = Email.newTask;
-		     	if (newTask != null){
+		     	if (newTask !== null){
 	     			render(newTask);
 					console.log('Task ' + newTask.id + ' picked');
 		     	}
@@ -88,7 +86,7 @@ function poll() {
 					console.log('No tasks pending');
 			     	setTimeout( function(){
 			     		poll();
-			     	}, 60000)
+			     	}, 60000);
 		     	}
 		     });
 		});
@@ -96,10 +94,10 @@ function poll() {
 }
 
 function render(Task) {
-	var subj = Task.message.subject
-	var htmlEmail = Task.message.html
-	batchId = Task.batchId
-	var textEmail = Task.message.text
+	var subj = Task.message.subject;
+	var htmlEmail = Task.message.html;
+	batchId = Task.batchId;
+	var textEmail = Task.message.text;
 	var transporter = nodemailer.createTransport({
 	    service: 'gmail',
 	    auth: {
@@ -123,20 +121,20 @@ function render(Task) {
         if (e.err){
         	if (e.err.code == 'ECONNREFUSED'){
 		    	client.end();
-		    	post_back('Connection') //Posting Connection to the result field of the task to tell that this is an "Connection Refused" kind of error.
+		    	post_back('Connection'); //Posting Connection to the result field of the task to tell that this is an "Connection Refused" kind of error.
 		    }
 		}
 	    else if (e.body) {
 		   	if (e.body.value.class === 'org.openqa.selenium.NoSuchElementException'){
 		   		client.end();
-		    	post_back('Element') //Posting Element to the result field of the task to tell that this is an "Element Not Found" kind of error.
+		    	post_back('Element'); //Posting Element to the result field of the task to tell that this is an "Element Not Found" kind of error.
 		    }
 	    }
 	    else {
 	    	client.end();
 	    	post_back('Unknown');
 	    }
-	})
+	});
 
 	client
 		.init()
@@ -155,7 +153,7 @@ function render(Task) {
 		.element('<body>').keys(['Down arrow'])
 		.element('<body>').keys(['Enter'])
 		.pause(5000)
-		.saveScreenshot(filepath_yahoo)
+		.saveScreenshot(filepath_yahoo);
 //*********************************************************************************************************
 
 	client.end(function () {
@@ -165,9 +163,9 @@ function render(Task) {
 	     		var base64data = base64_encode(filepath_yahoo);
 	     		post_back(base64data);
 	     	}
-     	}, 5000)
+     	}, 5000);
 	});
-};
+}
 
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
@@ -178,7 +176,7 @@ function base64_encode(file) {
 }
 
 function post_back(base64data) {
-	var postURL = prevemURL + '/api/PreviewTasks/update?where=%7B%22batchId%22%3A%22'+batchId+'%22%2C%22renderer%22%3A%22yahoo%22%7D'
+	var postURL = prevemURL + '/api/PreviewTasks/update?where=%7B%22batchId%22%3A%22'+batchId+'%22%2C%22renderer%22%3A%22yahoo%22%7D';
 	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
 	xmlhttp.open("POST", postURL + '&' + accessTokenUrlExtension);
 	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
